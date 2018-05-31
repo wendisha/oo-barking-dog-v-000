86f0fdd36f04cbef02e86d1993a6b566bd8289ea
Wendy Calderon <wendycalderon@gmail.com>
2018-05-31 18:17:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Dog 
-  def .name= (name)
+  def name= (name)
     @name = name 
   end 
   
-  def .name 
+  def name 
     @name 
   end 
   
-  def .bark
+  def bark
     puts "woof!"
   end
 end 
